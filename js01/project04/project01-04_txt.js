d065a0d2e9c8a3093e53f0e0cc50ebbd5625c90d
Piedmont0000 <dennymoe715@yahoo.com>
2024-10-15 19:18:47 -0400

    project 01-04

--- js01/project04/project01-04_txt.js
+++ js01/project04/project01-04_txt.js
@@ -7,6 +7,14 @@
 
     Filename: project01-04.js
 */
+var homeStreet = "1 Main St.";
+var homeCity = "Sicilia";
+var homeState = "MA";
+var homeCode = "02103";
+var workStreet = "15 Oak Ln.";
+var workCity = "Central City";
+var workState = "MA";
+var workCode = "02104";
 
 //define variables for home and work addresses
 
